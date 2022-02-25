32481f809f335706cb1d8a8c747b38e66b4937e1
LEE-YANG-JAE <33215984+LEE-YANG-JAE@users.noreply.github.com>
2022-02-25 11:35:40 +0900

    2022-02-25 Cryptopunk 클론 코딩 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
 import Header from './components/Header';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import PunkList from './components/PunkList';
+import Main from './components/Main';
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
+  useEffect(() => {
+    const getMyNfts = async () => { 
+      const openseaData = await axios.get(
+        'https://testnets-api.opensea.io/api/v1/assets?asset_contract_address=0xfb215CB1BeF45542208e9dA7Ecd42cFB9821210c&order_direction=asc&offset=0'
+        );
+      setPunkListData(openseaData.data.assets);
+    }
+    return getMyNfts();
+  }, []);
   return (
     <div className='app'>
       <Header/>
+      {
+        punkListData.length > 0 && (
+          <>
+          <Main selectedPunk={selectedPunk} punkListData={punkListData}/> 
+          <PunkList
+            punkListData={punkListData} setSelectedPunk={setSelectedPunk}
+          />
+        </>
+        )
+      }
+      
     </div>
   )
 }
